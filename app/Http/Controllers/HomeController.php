fd3c636626854a3f1199adb60f05bc6d861e521b
Pradana Setialana <pradana.setialana@gmail.com>
2021-12-09 13:27:39 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Individu;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $jumlahIndividu = Individu::count();
+
+        $dataIndividu = Individu::get();
+        
+        $balita = 0;
+
+        foreach ($dataIndividu as $individu) {
+            $umur = Carbon::parse($individu->tanggal_lahir)->age;
+            if ($umur <= 5) {
+                $balita = $balita + 1;
+            }
+        }
+
+        return view('home', ['jumlah_individu' => $jumlahIndividu, 'balita' => $balita ]);
     }
 }
